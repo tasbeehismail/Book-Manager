527bcda59301c8c911bd81376316262ddce137b8
tasbeehismail <tsbeehismaill@gmail.com>
2024-06-28 19:27:08 +0300

    Add error handling with examples and test them in Postman

--- src/modules/books/controller/book.js
+++ src/modules/books/controller/book.js
@@ -2,11 +2,13 @@ import Book from '../../../../database/models/book.js';
 
 export const addBook = async (req, res) => {
     try {
+        if(!req.body.title || !req.body.content || !req.body.author) {
+            return res.status(400).json({ message: 'Missing required fields: title, content and authorId' });
+        }
         const result = await Book.create(req.body);
         return res.status(201).json({ message: 'Book created successfully', data: result });
     } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: 'Internal server error' });
+        return res.status(500).json({ message: 'Internal server error.  Please try again later.' });
     }
 };
 
@@ -16,7 +18,7 @@ export const getBooks = async (req, res) => {
         .select('title content author publishedDate -_id');    
         return res.status(200).json({ message: 'Books fetched successfully', data: result });
     } catch (error) {
-        return res.status(500).json({ message: 'Internal server error' });
+        return res.status(500).json({ message: 'Internal server error.  Please try again later.' });
     }
 };
 
@@ -24,27 +26,36 @@ export const getBook = async (req, res) => {
     try {
         const result = await Book.findById(req.params.book_id)
         .select('title content author publishedDate -_id');
+        if(!result) {
+            return res.status(404).json({ message: 'Book not found' });
+        }
         return res.status(200).json({ message: 'Book fetched successfully', data: result });
     } catch (error) {
-        return res.status(500).json({ message: 'Internal server error' });
+        return res.status(500).json({ message: 'Internal server error.  Please try again later.' });
     }
 };
 
 export const updateBook = async (req, res) => {
     try {
         const result = await Book.findByIdAndUpdate(req.params.book_id, req.body, {new: true});
+        if(!result){
+            return res.status(404).json({ message: 'Book not found'});
+        }
         return res.status(200).json({ message: 'Book updated successfully', data: result });
     } catch (error) {
-        return res.status(500).json({ message: 'Internal server error' });
+        return res.status(500).json({ message: 'Internal server error.  Please try again later.' });
     }
 };
 
 export const deleteBook = async (req, res) => {
     try {
         const result = await Book.findByIdAndDelete(req.params.book_id);
+        if(!result){
+            return res.status(404).json({ message: 'Book not found'});
+        }
         return res.status(200).json({ message: 'Book deleted successfully', data: result });
     } catch (error) {
-        return res.status(500).json({ message: 'Internal server error' });
+        return res.status(500).json({ message: 'Internal server error.  Please try again later.' });
     }
 };
 
