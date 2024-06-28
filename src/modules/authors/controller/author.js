ead5a88d27e1c5bd5734f975f38ca2d6e7d80127
tasbeehismail <tsbeehismaill@gmail.com>
2024-06-28 19:26:57 +0300

    Add error handling with examples and test them in Postman

--- src/modules/authors/controller/author.js
+++ src/modules/authors/controller/author.js
@@ -2,6 +2,9 @@ import Author from '../../../../database/models/author.js';
 
 export const addAuthor = async (req, res) => {
     try {
+        if(!req.body.name) {
+            return res.status(400).json({ message: 'Missing required fields: name' });
+        }
         const result = await Author.create(req.body);
         return res.status(201).json({ message: 'Author created successfully', data: result });
     } catch (error) {
@@ -12,7 +15,8 @@ export const addAuthor = async (req, res) => {
 export const getAuthors = async (req, res) => {
     try {
         const result = await Author.find()
-        .select('name bio birthDate books -_id');    
+        .select('-__v -updatedAt -createdAt -_id')
+        .populate('books', 'title -_id'); 
         return res.status(200).json({ message: 'Authors fetched successfully', data: result });
     } catch (error) {
         return res.status(500).json({ message: 'Internal server error' });
@@ -22,7 +26,11 @@ export const getAuthors = async (req, res) => {
 export const getAuthor = async (req, res) => {
     try {
         const result = await Author.findById(req.params.author_id)
-        .select('name bio birthDate books -_id');
+        .select('-__v -updatedAt -createdAt -_id')
+        .populate('books', 'title -_id'); 
+        if(!result){
+            return res.status(404).json({ message: 'Author not found'});
+        }
         return res.status(200).json({ message: 'Author fetched successfully', data: result });
     } catch (error) {
         return res.status(500).json({ message: 'Internal server error' });
@@ -32,6 +40,9 @@ export const getAuthor = async (req, res) => {
 export const updateAuthor = async (req, res) => {
     try {
         const result = await Author.findByIdAndUpdate(req.params.author_id, req.body, {new: true});
+        if(!result){
+            return res.status(404).json({ message: 'Author not found'});
+        }
         return res.status(200).json({ message: 'Author updated successfully', data: result });
     } catch (error) {
         return res.status(500).json({ message: 'Internal server error' });
@@ -40,7 +51,10 @@ export const updateAuthor = async (req, res) => {
 
 export const deleteAuthor = async (req, res) => {
     try {
-        const result = await Author.findByIdAndDelete(req.params.uthor_id);
+        const result = await Author.findByIdAndDelete(req.params.author_id);
+        if(!result){
+            return res.status(404).json({ message: 'Author not found'});
+        }
         return res.status(200).json({ message: 'Author deleted successfully', data: result });
     } catch (error) {
         return res.status(500).json({ message: 'Internal server error' });
